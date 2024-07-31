a09872480c96bf4b4a9a0d4f251bb0a9c8792b0b
Bennett Petzold <dansecob.dev@gmail.com>
2024-07-31 01:53:57 -0400

    Allow for generic entry encode/decode
    Previous implementation was hard locked to bincode, this allows for any
    encode/decode pair on a serde compatible format.

--- src/entry/mod.rs
+++ src/entry/mod.rs
@@ -3,10 +3,15 @@ pub trait BackedEntryUnload {
     fn unload(&mut self);
 }
 
+use std::{cell::OnceCell, sync::OnceLock};
+
 use serde::{Deserialize, Deserializer, Serialize};
+
+use crate::utils::Once;
 
 #[cfg(feature = "async")]
 pub mod async_impl;
+pub mod formats;
 pub mod sync_impl;
 
 /// Indicates that a disk entry can be overwritten safely.
@@ -34,23 +39,27 @@ pub trait DiskOverwritable {}
 /// See [`BackedEntryBox`] for a heap-storing version.
 /// See [`BackedEntryArr`] for an array-specialized version.
 #[derive(Debug, Serialize, Deserialize)]
-pub struct BackedEntry<T, Disk: for<'df> Deserialize<'df>> {
+pub struct BackedEntry<T, Disk: for<'df> Deserialize<'df>, Coder> {
     #[serde(skip)]
-    value: Option<T>,
+    value: T,
     #[serde(deserialize_with = "backing_deserialize")]
     disk: Disk,
+    coder: Coder,
 }
 
-impl<T: Clone, Disk: for<'df> Deserialize<'df> + Clone> Clone for BackedEntry<T, Disk> {
+impl<T: Clone, Disk: for<'df> Deserialize<'df> + Clone, Coder: Clone> Clone
+    for BackedEntry<T, Disk, Coder>
+{
     fn clone(&self) -> Self {
         Self {
             value: self.value.clone(),
             disk: self.disk.clone(),
+            coder: self.coder.clone(),
         }
     }
 }
 
-impl<T, Disk: for<'de> Deserialize<'de>> BackedEntry<T, Disk> {
+impl<T, Disk: for<'de> Deserialize<'de>, Coder> BackedEntry<T, Disk, Coder> {
     pub fn get_disk(&self) -> &Disk {
         &self.disk
     }
@@ -69,29 +78,37 @@ where
     Deserialize::deserialize(deserializer)
 }
 
+pub type BackedEntryCell<T, Disk, Coder> = BackedEntry<OnceCell<T>, Disk, Coder>;
+pub type BackedEntryLock<T, Disk, Coder> = BackedEntry<OnceLock<T>, Disk, Coder>;
+
 /// Specialized typedef of [`BackedEntry`] for non-pointer types.
 ///
 /// # Example
 ///
 /// ```rust
 /// use std::fs::{File, remove_file};
-/// use backed_data::entry::BackedEntryBox;
+/// use backed_data::entry::{
+///     BackedEntryBox,
+///     formats::BincodeCoder,
+/// };
 ///
 /// let FILENAME = std::env::temp_dir().join("example_box");
-///
 /// // Write array to file
-/// let mut writer: BackedEntryBox<str, _> = BackedEntryBox::new(FILENAME.clone());
+/// let mut writer: BackedEntryBox<str, _, _> = BackedEntryBox::new(FILENAME.clone(),
+///     BincodeCoder{});
 /// writer.write_unload("HELLO I AM A STRING").unwrap();
 /// drop(writer);
 ///
 /// // Read array from file
-/// let mut sparse: BackedEntryBox<str, _> = BackedEntryBox::new(FILENAME.clone());
+/// let mut sparse: BackedEntryBox<str, _, _> = BackedEntryBox::new(FILENAME.clone(),
+///     BincodeCoder{});
 /// assert_eq!(sparse.load().unwrap().as_ref(), "HELLO I AM A STRING");
 ///
 /// // Cleanup
 /// remove_file(FILENAME).unwrap();
 /// ```
-pub type BackedEntryBox<T, Disk> = BackedEntry<Box<T>, Disk>;
+pub type BackedEntryBox<T, Disk, Coder> = BackedEntryCell<Box<T>, Disk, Coder>;
+pub type BackedEntryBoxLock<T, Disk, Coder> = BackedEntryLock<Box<T>, Disk, Coder>;
 
 /// Specialized typedef of [`BackedEntry`] for arrays.
 ///
@@ -99,40 +116,54 @@ pub type BackedEntryBox<T, Disk> = BackedEntry<Box<T>, Disk>;
 ///
 /// ```rust
 /// use std::fs::{File, remove_file};
-/// use backed_data::entry::BackedEntryArr;
+/// use backed_data::entry::{
+///     BackedEntryArr,
+///     formats::BincodeCoder,
+/// };
 ///
 /// let FILENAME = std::env::temp_dir().join("example_array");
 ///
 /// // Write array to file
-/// let mut writer: BackedEntryArr<i32, _> = BackedEntryArr::new(FILENAME.clone());
+/// let mut writer: BackedEntryArr<u8, _, _> = BackedEntryArr::new(FILENAME.clone(),
+///     BincodeCoder{});
 /// writer.write_unload([1, 2, 3]).unwrap();
 /// drop(writer);
 ///
 /// // Read array from file
-/// let mut sparse: BackedEntryArr<i32, _> = BackedEntryArr::new(FILENAME.clone());
+/// let mut sparse: BackedEntryArr<u8, _, _> = BackedEntryArr::new(FILENAME.clone(),
+///     BincodeCoder{});
 /// assert_eq!(sparse.load().unwrap().as_ref(), [1, 2, 3]);
 ///
 /// // Cleanup
 /// remove_file(FILENAME).unwrap();
 /// ```
-pub type BackedEntryArr<T, Disk> = BackedEntryBox<[T], Disk>;
+pub type BackedEntryArr<T, Disk, Coder> = BackedEntryBox<[T], Disk, Coder>;
+pub type BackedEntryArrLock<T, Disk, Coder> = BackedEntryBoxLock<[T], Disk, Coder>;
 
 // ----- Initializations ----- //
 
-impl<T, Disk: for<'de> Deserialize<'de>> BackedEntry<T, Disk> {
-    pub fn new(disk: Disk) -> Self {
-        Self { value: None, disk }
+impl<T: Once, Disk: for<'de> Deserialize<'de>, Coder> BackedEntry<T, Disk, Coder> {
+    pub fn new(disk: Disk, coder: Coder) -> Self {
+        Self {
+            value: T::new(),
+            disk,
+            coder,
+        }
     }
 }
 
-impl<T, Disk: for<'de> Deserialize<'de>> AsRef<BackedEntry<T, Disk>> for BackedEntry<T, Disk> {
-    fn as_ref(&self) -> &BackedEntry<T, Disk> {
+impl<T, Disk: for<'de> Deserialize<'de>, Coder> AsRef<BackedEntry<T, Disk, Coder>>
+    for BackedEntry<T, Disk, Coder>
+{
+    fn as_ref(&self) -> &BackedEntry<T, Disk, Coder> {
         self
     }
 }
 
-impl<T, Disk: for<'de> Deserialize<'de>> AsMut<BackedEntry<T, Disk>> for BackedEntry<T, Disk> {
-    fn as_mut(&mut self) -> &mut BackedEntry<T, Disk> {
+impl<T, Disk: for<'de> Deserialize<'de>, Coder> AsMut<BackedEntry<T, Disk, Coder>>
+    for BackedEntry<T, Disk, Coder>
+{
+    fn as_mut(&mut self) -> &mut BackedEntry<T, Disk, Coder> {
         self
     }
 }
