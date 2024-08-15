2a15354ee518d82f092d53ede0a19bc2ad2a92c3
Bennett Petzold <dansecob.dev@gmail.com>
2024-08-15 13:59:51 -0400

    Add async simd adapter example

--- examples/async_simd.rs
+++ examples/async_simd.rs
@@ -1,31 +1,39 @@
 #![cfg(all(feature = "simd_json", feature = "async"))]
 
-fn main() {}
+use std::{collections::HashMap, env::temp_dir};
 
-/*
-use tokio::task::spawn_blocking;
+use backed_data::{
+    entry::{
+        adapters::{DecodeBg, EncodeBg, SyncCoderAsAsync},
+        disks::{Plainfile, ReadDisk, WriteDisk},
+        formats::{AsyncDecoder, AsyncEncoder, SimdJsonCoder},
+    },
+    utils::blocking::tokio_blocking,
+};
+use tokio::fs::remove_dir_all;
 
 #[tokio::main]
 async fn main() {
     let backing_file = temp_dir().join("backed_array_async_simd");
     let data: HashMap<usize, usize> = (0..10_000).map(|x| (x, x * 2)).collect();
 
-    /*
-    coder
-        .encode(&data, &mut disk.write_disk().unwrap())
-        .await
-        .unwrap();
-    */
+    let disk = Plainfile::new(backing_file.clone());
 
-    /*
-    let coder = SyncCoderAsAsync::<_, Mmap, _, _, _>::new(
-        SimdJsonCoder::<HashMap<usize, usize>>::default(),
-        |x: EncodeBg<SimdJsonCoder<HashMap<_, _>>, Mmap>| ready(x.call()),
+    let coder = SyncCoderAsAsync::<_, Plainfile, _, _, _, _>::new(
+        SimdJsonCoder::<_>::default(),
+        |x: DecodeBg<_, _>| unsafe { tokio_blocking(x) },
+        |x: EncodeBg<_, _>| unsafe { tokio_blocking(x) },
     );
 
-    coder.encode(&data, &mut disk).await.unwrap();
-    */
+    // Encode asynchronously via a background thread
+    coder
+        .encode(&data, disk.write_disk().unwrap())
+        .await
+        .unwrap();
 
-    let _ = remove_dir_all(backing_file);
+    // Decode asynchronously via a background thread
+    let decoded = coder.decode(disk.read_disk().unwrap()).await.unwrap();
+    assert_eq!(data, decoded);
+
+    let _ = remove_dir_all(backing_file).await;
 }
-*/
