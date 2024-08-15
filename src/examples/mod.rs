98cf375cd2d85bd29321fb3b214170514679cc60
Bennett Petzold <dansecob.dev@gmail.com>
2024-08-15 13:59:51 -0400

    Add async simd adapter example

--- src/examples/mod.rs
+++ src/examples/mod.rs
@@ -1,7 +1,10 @@
-//! All examples are re-exported from `$CRATE_ROOT/examples`, and can be executed via `cargo run
-//! --example NAME` with the necessary feature flags.
+//! Example usage re-exports.
+//!
+//! All examples are re-exported from `$CRATE_ROOT/examples`, and can be
+//! executed via `cargo run --example NAME` with the necessary feature flags.
 
 pub mod async_mmap;
+pub mod async_simd;
 pub mod large_reencode;
 pub mod mmap;
 pub mod remote_loading;
