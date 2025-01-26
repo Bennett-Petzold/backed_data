dbc10284f7bd7af035475245ecab8bd8ce58dfe2
Bennett-Petzold <46354948+Bennett-Petzold@users.noreply.github.com>
2025-01-26 16:52:40 +0000

    Deploying to gh-pages from @ Bennett-Petzold/backed_data@2f21b10a69565059094502f8913a7dcbc9956a34 🚀

--- docs/tower/sidebar-items.js
+++ docs/tower/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["service_fn"],"mod":["builder","layer","make","util"],"struct":["ServiceBuilder"],"trait":["Layer","MakeService","Service","ServiceExt"],"type":["BoxError"]};
+window.SIDEBAR_ITEMS = {"fn":["service_fn"],"mod":["builder","layer","timeout","util"],"struct":["ServiceBuilder"],"trait":["Layer","Service","ServiceExt"],"type":["BoxError"]};
